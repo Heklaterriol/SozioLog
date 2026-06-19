5a262fa97aa0ada620bbb92917529051d3dffece
Hekla Terriol <webstuff@sternenaugen.net>
2026-06-19 22:20:33 +0200

    add PHPMailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'logbuch/soziokratie',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8978fe3c4115db709ac5aa75ac8a1957781751e7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'logbuch/soziokratie' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8978fe3c4115db709ac5aa75ac8a1957781751e7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.1.1',
+            'version' => '7.1.1.0',
+            'reference' => '1bc1716a507a65e039d4ac9d9adebbbd0d346e15',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
